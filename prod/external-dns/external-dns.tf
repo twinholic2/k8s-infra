e6de1f5090bbb9ff5f1cdd3638cb04a60a96198f
twinholic2 <odark@naver.com>
2024-04-12 21:40:26 +0900

    oidc arn modify

--- prod/external-dns/external-dns.tf
+++ prod/external-dns/external-dns.tf
@@ -21,7 +21,7 @@ resource "kubernetes_service_account" "alb_service_account" {
 }
 
 data "aws_iam_openid_connect_provider" "cluster_provider" {
-  arn = "arn:aws:iam::682935334295:oidc-provider/oidc.eks.ap-northeast-2.amazonaws.com/id/DC8D53DA4D801C71704542DBC947961D"
+  arn = "${data.aws_iam_openid_connect_provider.cluster_provider.arn}"
 }
 
 
