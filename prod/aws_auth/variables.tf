667efa8542cbc653c911d88d603ea5408f56ae56
twinholic2 <odark@naver.com>
2024-04-12 21:00:48 +0900

    variables modify

---
+++ prod/aws_auth/variables.tf
@@ -0,0 +1,10 @@
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key Id"
+  type    = string
+}
+
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Access Key"
+  type    = string
+}
